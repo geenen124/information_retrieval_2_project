b6d499b303e168625605daf2c39f3282b9f39dbc
diana <diana3372@gmail.com>
2018-11-26 11:06:08 +0100

    move to next batch

--- seqGAN/generator.py
+++ seqGAN/generator.py
@@ -5,8 +5,7 @@ import torch.nn.functional as F
 
 from training_ptr_gen.model import Model
 from data_util import config
-from helpers import random_from_data
-from training_ptr_gen.train_util import get_input_from_batch, get_output_from_batch
+from helpers import random_batch
 
 
 class Generator(nn.Module):
@@ -15,50 +14,33 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
         self.seqseq_model = Model(model_file_path=None)
 
-    def init_hidden(self, batch_size=1):
-        # h = autograd.Variable(torch.zeros(1, batch_size, self.hidden_dim))
-        h = nn.Linear(config.hidden_dim * 2, config.hidden_dim * 2, bias=False) # ToDo: Do we want to initialize with the hidden state from random data?
+    def x(n_samples, vocab):
 
-        if config.use_gpu:
-            return h.cuda()
-        else:
-            return h
+        # Grab ramdom subset of trainng data
+        batch = random_batch(n_samples, vocab)
 
-    def sample(self, num_samples, vocab):
-        """ 
-        Samples the network and returns num_samples samples of length max_seq_len.
+        #rewards = helpers.get_rewards(batch) # mentira
 
-        Outputs: samples, hidden
-            - samples: num_samples x max_seq_length (a sampled sequence in each row)
-        """
-
-        batch = random_from_data(num_samples, vocab)
-
+        # Encoder pass
         enc_batch, enc_padding_mask, enc_lens, enc_batch_extend_vocab, extra_zeros, c_t_1, coverage = \
             get_input_from_batch(batch, config.use_gpu)
+
+        encoder_outputs, encoder_feature, encoder_hidden = self.model.encoder(enc_batch, enc_lens)
+        s_t_1 = self.model.reduce_state(encoder_hidden)
+
+        # Decoder pass
+        # Run beam search to get best Hypothesis
+        best_summary = self.beam_search(batch)
+
+
+
+
+
         dec_batch, dec_padding_mask, max_dec_len, dec_lens_var, target_batch = \
-            get_output_from_batch(batch, config.use_gpu)
+            get_output_from_batch(batch, config.use_gpu) # I think i only want the target_batch
 
-        print(enc_batch)
-        print(dec_batch)
+        return
 
-        # samples = torch.zeros(num_samples, config.max_enc_steps).type(torch.LongTensor) # ToDo: Verify max seq length
-
-        # h = self.init_hidden(num_samples)
-        # inp = torch.LongTensor([start_letter]*num_samples)
-
-        # if config.use_gpu:
-        #     samples = samples.cuda()
-        #     inp = inp.cuda()
-
-        # for i in range(config.max_enc_steps):
-        #     out, h = self.seqseq_model(inp, h)               # out: num_samples x vocab_size
-        #     out = torch.multinomial(torch.exp(out), 1)  # num_samples x 1 (sampling from each row)
-        #     samples[:, i] = out.view(-1).data
-
-        #     inp = out.view(-1)
-
-        # return samples
 
     def batchPGLoss(self, inp, target, reward):
         """
@@ -88,3 +70,141 @@ class Generator(nn.Module):
 
         return loss/batch_size
 
+
+
+class BeamSearch(object):
+    def __init__(self, vocab, model):
+        self.vocab = vocab
+        self.batcher = Batcher(config.decode_data_path, self.vocab, mode='decode',
+                               batch_size=config.batch_size, single_pass=True) 
+        time.sleep(15)
+
+        self.model = model
+        # Todo: remove this todo when we changed back the mode to training.
+        self.encoder = encoder.eval()
+        self.decoder = decoder.eval()
+        self.reduce_state = reduce_state.eval()
+
+    def sort_beams(self, beams):
+        return sorted(beams, key=lambda h: h.avg_log_prob, reverse=True)
+
+
+    def decode(self, num_samples):
+        batch = self.batcher.next_batch()
+        
+        for _ in num_samples:
+            # Run beam search to get best Hypothesis
+            best_summary = self.beam_search(batch)
+
+            # Extract the output ids from the hypothesis and convert back to words
+            output_ids = [int(t) for t in best_summary.tokens[1:]]
+            decoded_words = data.outputids2words(output_ids, self.vocab, None)
+
+            # Remove the [STOP] token from decoded_words, if necessary
+            try:
+                fst_stop_idx = decoded_words.index(data.STOP_DECODING)
+                decoded_words = decoded_words[:fst_stop_idx]
+            except ValueError:
+                decoded_words = decoded_words
+
+            original_abstract_sents = batch.original_abstracts_sents[0]
+
+            batch = self.batcher.next_batch()
+
+        print("Finished sampling.")
+
+
+    def beam_search(self, batch):
+        #batch should have only one example
+        enc_batch, enc_padding_mask, enc_lens, enc_batch_extend_vocab, extra_zeros, c_t_0, coverage_t_0 = \
+            get_input_from_batch(batch, use_cuda)
+
+        encoder_outputs, encoder_feature, encoder_hidden = self.model.encoder(enc_batch, enc_lens)
+        s_t_0 = self.model.reduce_state(encoder_hidden)
+
+        dec_h, dec_c = s_t_0 # 1 x 2*hidden_size
+        dec_h = dec_h.squeeze()
+        dec_c = dec_c.squeeze()
+
+        #decoder batch preparation, it has beam_size example initially everything is repeated
+        beams = [Beam(tokens=[self.vocab.word2id(data.START_DECODING)],
+                      log_probs=[0.0],
+                      state=(dec_h[0], dec_c[0]),
+                      context = c_t_0[0],
+                      coverage=(coverage_t_0[0] if config.is_coverage else None))
+                 for _ in range(config.beam_size)]
+        results = []
+        steps = 0
+        while steps < config.max_dec_steps and len(results) < config.beam_size:
+            latest_tokens = [h.latest_token for h in beams]
+            latest_tokens = [t if t < self.vocab.size() else self.vocab.word2id(data.UNKNOWN_TOKEN) \
+                             for t in latest_tokens]
+            y_t_1 = Variable(torch.LongTensor(latest_tokens))
+            if use_cuda:
+                y_t_1 = y_t_1.cuda()
+            all_state_h =[]
+            all_state_c = []
+
+            all_context = []
+
+            for h in beams:
+                state_h, state_c = h.state
+                all_state_h.append(state_h)
+                all_state_c.append(state_c)
+
+                all_context.append(h.context)
+
+            s_t_1 = (torch.stack(all_state_h, 0).unsqueeze(0), torch.stack(all_state_c, 0).unsqueeze(0))
+            c_t_1 = torch.stack(all_context, 0)
+
+            coverage_t_1 = None
+            if config.is_coverage:
+                all_coverage = []
+                for h in beams:
+                    all_coverage.append(h.coverage)
+                coverage_t_1 = torch.stack(all_coverage, 0)
+
+            final_dist, s_t, c_t, attn_dist, p_gen, coverage_t = self.model.decoder(y_t_1, s_t_1,
+                                                        encoder_outputs, encoder_feature, enc_padding_mask, c_t_1,
+                                                        extra_zeros, enc_batch_extend_vocab, coverage_t_1, steps)
+
+            topk_log_probs, topk_ids = torch.topk(final_dist, config.beam_size * 2)
+
+            dec_h, dec_c = s_t
+            dec_h = dec_h.squeeze()
+            dec_c = dec_c.squeeze()
+
+            all_beams = []
+            num_orig_beams = 1 if steps == 0 else len(beams)
+            for i in range(num_orig_beams):
+                h = beams[i]
+                state_i = (dec_h[i], dec_c[i])
+                context_i = c_t[i]
+                coverage_i = (coverage_t[i] if config.is_coverage else None)
+
+                for j in range(config.beam_size * 2):  # for each of the top 2*beam_size hyps:
+                    new_beam = h.extend(token=topk_ids[i, j].item(),
+                                   log_prob=topk_log_probs[i, j].item(),
+                                   state=state_i,
+                                   context=context_i,
+                                   coverage=coverage_i)
+                    all_beams.append(new_beam)
+
+            beams = []
+            for h in self.sort_beams(all_beams):
+                if h.latest_token == self.vocab.word2id(data.STOP_DECODING):
+                    if steps >= config.min_dec_steps:
+                        results.append(h)
+                else:
+                    beams.append(h)
+                if len(beams) == config.beam_size or len(results) == config.beam_size:
+                    break
+
+            steps += 1
+
+        if len(results) == 0:
+            results = beams
+
+        beams_sorted = self.sort_beams(results)
+
+        return beams_sorted[0]
